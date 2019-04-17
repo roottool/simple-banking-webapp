45419dee088ad03028d1fe96edf5386d55835dc5
roottool <smart.dog.lilienthal@gmail.com>
2019-04-18 02:34:55 +0900

    Created a logic of sending data to Firestore

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import Express from 'express';
-import bodyParser from 'body-parser';
+import BodyParser from 'body-parser';
 import firebase from 'firebase';
 
 import { BalanceCreateRequestBody, BalanceCreateResponseBody } from './models/balances';
@@ -7,14 +7,15 @@ import config from './firebase/config';
 
 firebase.initializeApp(config);
 const db = firebase.firestore();
+const balances = 'Balances';
 const app = Express();
 
 app.use(
-  bodyParser.urlencoded({
+  BodyParser.urlencoded({
     extended: true,
   })
 );
-app.use(bodyParser.json());
+app.use(BodyParser.json());
 
 if (process.env.NODE_ENV === 'development') {
   app.listen(3000, () => {
@@ -23,7 +24,7 @@ if (process.env.NODE_ENV === 'development') {
   });
 }
 
-app.post('/balances', (req, res) => {
+app.post('/balances', async (req, res) => {
   const recievedData: BalanceCreateRequestBody = req.body;
   const sendData: BalanceCreateResponseBody = {
     balance: {
@@ -33,7 +34,15 @@ app.post('/balances', (req, res) => {
     },
   };
 
-  return res.send(sendData);
+  return await db
+    .collection(balances)
+    .add(sendData)
+    .then(() => {
+      res.send(sendData);
+    })
+    .catch(() => {
+      res.status(400);
+    });
 });
 
 app.get('/balances/:balanceId', (req, res) => {
