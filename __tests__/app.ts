b6c3316c20806d7933f81bc46f369ec308931c6b
roottool <smart.dog.lilienthal@gmail.com>
2019-04-18 01:03:05 +0900

    Created POST method

--- __tests__/app.ts
+++ __tests__/app.ts
@@ -1,9 +1,25 @@
 import app from '../src/app';
 import request from 'supertest';
 
+import { BalanceCreateRequestBody } from '../src/models/balances';
+
+describe('POST /balences', () => {
+  it('Receive data after data was sent.', async () => {
+    const sendData: BalanceCreateRequestBody = {
+      balanceId: 0,
+      amount: 100,
+    };
+    const result = await request(app)
+      .post('/balances')
+      .send(sendData);
+    expect(result.status).toBe(200);
+    expect(result.body).toBeTruthy();
+  });
+});
+
 describe('GET /balences/balancesId', () => {
   it('If the balance does not exist, throw 404 error.', async () => {
     const result = await request(app).get('/balances/1');
-    expect(result.status).toEqual(404);
+    expect(result.status).toBe(404);
   });
 });
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,39 @@
 import Express from 'express';
+import bodyParser from 'body-parser';
 import firebase from 'firebase';
 
+import { BalanceCreateRequestBody, BalanceCreateResponseBody } from './models/balances';
 import config from './firebase/config';
 
 firebase.initializeApp(config);
 const db = firebase.firestore();
 const app = Express();
 
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === 'development') {
+  app.listen(3000, () => {
+    // tslint:disable-next-line: no-console
+    console.log('Ready!');
+  });
+}
+
 app.post('/balances', (req, res) => {
-  return res.status(404).send('Not found');
+  const recievedData: BalanceCreateRequestBody = req.body;
+  const sendData: BalanceCreateResponseBody = {
+    balance: {
+      id: recievedData.balanceId,
+      amount: recievedData.amount,
+      createdAt: new Date(),
+    },
+  };
+
+  return res.send(sendData);
 });
 
 app.get('/balances/:balanceId', (req, res) => {
